53e26e990790f4526600d8895d00a3ceb93f7b19
Thien-Y Le <thile@microsoft.com>
2020-01-24 23:20:39 -0800

    First commit

---
+++ MyHouseAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSEOMyRentalApp
+{
+    class MyHouseAccount
+    {
+        public char Address { get; set; }
+        public DateTime YearBuilt { get; set; }
+        public int PurchasePrice { get; set; }
+        public int MarketValue { get; set; }
+        public char PropertyType { get; set; }
+
+    }
+}
