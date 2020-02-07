d8be9ab9e3c3aad75d5d19d7045afa95834fa32a
Thien-Y Le <thile@microsoft.com>
2020-02-06 23:58:07 -0800

    Second commit for 1/24 class content

--- MyHouseAccount.cs
+++ MyHouseAccount.cs
@@ -4,13 +4,71 @@ using System.Text;
 
 namespace CSEOMyRentalApp
 {
+    enum TypeProperty
+    { 
+        Single,
+        Multifamily,
+        Condo,
+        Townhome }
+    //Single line comment
+    /*Practice line
+     * Practice line
+     * Practice line
+     */
+     /// <summary>
+     /// This is an application that will allow you to keep track of your house rental expenses. 
+     /// </summary>
     class MyHouseAccount
     {
-        public char Address { get; set; }
-        public DateTime YearBuilt { get; set; }
+        private static int lastAccountNumber = 0;
+
+        #region Properties
+        /// <summary>
+        /// Account number for the rental place
+        /// </summary>
+        public int AccountNumber { get; private set; }
+        public DateTime CreatedDate{ get; private set; }
+        public string Address { get; set; }
+        public int YearBought { get; set; }
         public int PurchasePrice { get; set; }
         public int MarketValue { get; set; }
-        public char PropertyType { get; set; }
+        public TypeProperty PropertyType { get; set;}
+        public int TotalRentalIncome{ get; private set; }
+        #endregion
 
+        #region Method
+        /// <summary>
+        /// Deposit money into account
+        /// </summary>
+        /// <param name="amount">Amount to deposit</param>
+        public void Deposit(int amount)
+        {
+            //Balance = Balance + amount
+            TotalRentalIncome += amount;
+         }
+        /// <summary>
+        /// Costs from rental property such as fixing roof, hiring plumber, etc. 
+        /// </summary>
+        /// <param name="amount">Amount it costs to maintain the rental property</param>
+        /// <returns>Total rent paid - cost to maintain rental property</returns>
+        public int Withdraw(int amount)
+        {
+            TotalRentalIncome -= amount;
+            return TotalRentalIncome;
+         }
+        #endregion
+
+        #region Constructor
+        //Default constructor
+        public MyHouseAccount()
+        {
+            //lastAccountNumber = lastAccountNumber + 1; 
+            //lastAccountNumber++; ++ means increment by 1
+            //AccountNumber = lastAccountNumber;
+            AccountNumber = ++lastAccountNumber;
+            CreatedDate = DateTime.UtcNow;
+
+        }
+        #endregion
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace CSEOMyRentalApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //variable name; everything is a variable when you store data
+            var myAccount = new MyHouseAccount();
+            myAccount.Address = "ABC Street";
+            myAccount.YearBought = 1984;
+            myAccount.PurchasePrice = 500000;
+            myAccount.MarketValue = 600000;
+            myAccount.PropertyType = TypeProperty.Single;
+            myAccount.Deposit(1000);
+            Console.WriteLine($"AN:{myAccount.AccountNumber}, AD:{myAccount.Address}, YearBought: {myAccount.YearBought}, " +
+                $"PurchasePrice: {myAccount.PurchasePrice:C}, MarketValue: {myAccount.MarketValue:C}, PropertyType: {myAccount.PropertyType}, " +
+                $"TotalRentalIncome: {myAccount.TotalRentalIncome:C}, CreatedDate: {myAccount.CreatedDate}");
+
+
+            var myAccount2 = new MyHouseAccount();
+            myAccount2.Address = "XYZ Avenue";
+            myAccount2.YearBought = 1990;
+            myAccount2.PurchasePrice = 300000;
+            myAccount2.MarketValue = 400000;
+            myAccount2.PropertyType = TypeProperty.Multifamily;
+            Console.WriteLine($"AN:{myAccount2.AccountNumber}, AD:{myAccount2.Address}, YearBought: {myAccount2.YearBought}, " +
+               $"PurchasePrice: {myAccount2.PurchasePrice:C}, MarketValue: {myAccount2.MarketValue:C}, PropertyType: {myAccount2.PropertyType}, " +
+               $"TotalRentalIncome: {myAccount2.TotalRentalIncome:C}, CreatedDate: {myAccount2.CreatedDate}");
+
         }
     }
 }
